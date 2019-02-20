3f7b62ce891c6d193310931c48cae86c9d01e5a7
balajigan <balaji_inf@yahoo.com>
2019-02-19 19:46:31 -0600

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,18 @@
+# starting point for main
+
+class Main:
+
+  def __init__(self):
+    print("Main initialized ...")
+
+  def display(self):
+    print("Display is called ...")
+
+# End of Main class
+
+# Flow starts here
+
+main = new Main()
+
+main.display()
+
